1f3271590d7cd24d40412ee17ae7c2790bf3231d
Fairuz <fairuz.sulaiman@gmail.com>
2023-11-30 18:18:24 +0800

    Add project files.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Training.Controllers
+{
+	public class BookController : Controller
+	{
+		//public String Index() 
+		//{
+		//	return "Ini adalah default saya ";
+		//}
+
+		public IActionResult Index() { 
+			return View(); 		
+		}
+
+		public string Welcome(string name, int numtimes)
+		{
+			return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numtimes}");
+		}
+
+		public IActionResult About()
+		{
+			return View();
+		}
+	}
+}
